0bececbaef1ea0a48d5b7bba257ccb78b0b128ca
Looloo414 <alexafaulkison@gmail.com>
2020-11-23 18:17:11 -0700

    All buttons display'
    clear
    exit
    cd ..
    escape
    exit
    clear
    cd /app.js
    cd ~
    :

--- js/app.js
+++ js/app.js
@@ -3,8 +3,6 @@
 
 
 // -------------------VARIABLES--------------------
-
-let weatherAPI = "https://www.metaweather.com/api/location/search/?query=denver"
 let sunsetAPI = "https://api.sunrise-sunset.org/json?lat=36.7201600&lng=-4.4203400&callback=mycallback"
 
 // ----------------CACHED ELEMENTS------------------
@@ -13,59 +11,74 @@ const den = document.getElementById('denver');
 const dallas = document.getElementById('dallas');
 const la = document.getElementById('los-ang');
 const container = document.querySelector('.container')
-let dallasInfo = [] 
+let dallasInfo = []
+let denInfo = []
+let laInfo = []
 
 
 // ----------------EVENT LISTENERS-------------------
 den.addEventListener('click', () => {
     fetch("https://cors-anywhere.herokuapp.com/metaweather.com/api/location/2391279/2020/11/23/")
-    .then(response => response.json())
-    .then((data) => {
-        let totalData = {}
-    container.innerHTML = data[0].weather_state_name
-    container.innerHTML = data[0].visibility
-    container.innerHTML = data[0].wind_speed
-    container.innerHTML = data[0].wind_direction
-    })
-
-    .catch((err) => {console.log(err)})
-
+        .then(response => response.json())
+        .then((data) => {
+            let denData = {}
+            denData['visibility'] = data[0].visibility
+            denData['wind_speed'] = data[0].wind_speed
+            denData['wind_direction'] = data[0].wind_direction
+            denData['weather_state_name'] = data[0].weather_state_name
+            denInfo.push(denData)
+            console.log(denInfo)
+            renderDen()
+        })
+        .catch((err) => {
+            console.log(err)
+        })
 })
+
 dallas.addEventListener('click', () => {
     fetch("https://cors-anywhere.herokuapp.com/metaweather.com/api/location/2388929/2020/11/23/")
-    .then(response => response.json())
-    .then((data) => {
-    let dallasData = {}
-        dallasData['location'] = 'Dallas'
-        dallasData['visibility'] = data[0].visibility
-        dallasData['wind_speed'] = data[0].wind_speed
-        dallasData['wind_direction'] = data[0].wind_direction
-        dallasData['weather_state_name'] = data[0].weather_state_name
-        dallasInfo.push(dallasData)
-        // container.innerHTML = dallasInfo
-        render()
-})
-.catch(err => console.log(err))
+        .then(response => response.json())
+        .then((data) => {
+            let dallasData = {}
+            dallasData['visibility'] = data[0].visibility
+            dallasData['wind_speed'] = data[0].wind_speed
+            dallasData['wind_direction'] = data[0].wind_direction
+            dallasData['weather_state_name'] = data[0].weather_state_name
+            dallasData['location'] = 'Dallas'
+            dallasInfo.push(dallasData)
+            render()
+        })
+        .catch((err) => {
+            console.log(err)
+        })
 
 })
 
 la.addEventListener('click', () => {
     fetch("https://cors-anywhere.herokuapp.com/metaweather.com/api/location/2442047/2020/11/23/")
-    .then(response => response.json())
-    .then((data) => {
-    console.log(data[0].weather_state_name)
-})
-.catch(err => console.log(err))
+        .then(response => response.json())
+        .then((data) => {
+            let laData = {}
+            laData['visibility'] = data[0].visibility
+            laData['wind_speed'] = data[0].wind_speed
+            laData['wind_direction'] = data[0].wind_direction
+            laData['weather_state_name'] = data[0].weather_state_name
+            laInfo.push(laData)
+            renderLA()
+    })
+    .catch((err) => {
+        console.log(err)
+    })
 
 })
 
-function appendDiv(visibility, wind_direction, wind_speed, weather_state_name, location, idx) {
+function appendDiv(visibility, wind_direction, wind_speed, weather_state_name, idx) {
     let newDiv = document.createElement("div")
     newDiv.innerHTML = `
-                        <div class="card h-100" >
+                        <div class="card h-100">
                             <div class="card-body">
                                 <blockquote class="blockquote mb-0">
-                                    <p>Location: ${location}</p>
+                            
                                     <p>Visibility:${visibility}</p>
                                     <p>Cloud Coverage: ${weather_state_name}</p>
                                     <p>Wind direction: ${wind_direction}</p>
@@ -80,13 +93,38 @@ function appendDiv(visibility, wind_direction, wind_speed, weather_state_name, l
 }
 
 function deleteQuote(idx) {
-    quotes.splice(idx, 1)
+   dallasInfo.splice(idx, 1)
     render()
-  }
+}
+
+function deleteQuote(idx) {
+    denInfo.splice(idx, 1)
+     renderDen()
+ }
+
+ function deleteQuote(idx) {
+    laInfo.splice(idx, 1)
+     renderLA()
+ }
+
 
 function render() {
     container.innerHTML = ''
     dallasInfo.forEach((x, idx) => {
+        appendDiv(x['visibility'], x['wind_speed'], x['wind_direction'], x['weather_state_name'], idx)
+    })
+}
+
+function renderDen() {
+    container.innerHTML = ''
+    denInfo.forEach((x, idx) => {
+        appendDiv(x['visibility'], x['wind_speed'], x['wind_direction'], x['weather_state_name'], idx)
+    })
+}
+
+function renderLA() {
+    container.innerHTML = ''
+    laInfo.forEach((x, idx) => {
         appendDiv(x['visibility'], x['wind_speed'], x['wind_direction'], x['weather_state_name'], idx)
     })
 }
